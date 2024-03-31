4f4c58df48c6a1129b3eb1c329cfc562b4c49130
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-03-30 22:59:55 -0400

    Dynamic header

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
+import React from "react";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: { type } }:
+  SearchParamProps) => {
+    const transformation = transformationTypes[type];
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <Header
+    title={transformation.title}
+    subtitle={transformation.subTitle} />
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
